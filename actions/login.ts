a0b4105959ffa077440ea4d07dde698d913b7e9a
Arnab-pixel <arnabchaudhuri2002@gmail.com>
2024-08-27 18:28:52 +0530

    Before next auth

---
+++ actions/login.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { LoginSchema } from "@/schemas";
+
+import * as z from "zod";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFeilds = LoginSchema.safeParse(values);
+
+  if (!validatedFeilds.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  return { success: "Email sent!" };
+};
